c0db81076dd0a6ab360578e94f342ab9501bf7a7
Sabrina Li <sabrinali0312@gmail.com>
2019-09-10 12:56:19 -0400

    switch to production

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 const PORT = process.env.PORT || 3001;
-var syncOptions = { force: true };
+var syncOptions = { force: false };
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb` nad `developmentdb`
